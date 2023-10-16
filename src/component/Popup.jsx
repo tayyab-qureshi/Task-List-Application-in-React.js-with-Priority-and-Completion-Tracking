216fee473bf2e20280bb1bcff528caa2d471e121
tayyab-qureshi <qureshitayyab05@gmail.com>
2023-10-16 23:17:56 +0500

    menu yaoo

--- src/component/Popup.jsx
+++ src/component/Popup.jsx
@@ -23,56 +23,61 @@ const Popup = ({ popup, setPopup,handleSubmitData }) => {
           left: "50%",
           top: "50%",
           transform: "translate(-50%, -50%)",
+          backgroundColor: "var(--main-clr)"
         }}
-        className="popup w-4/12  rounded h-[80vh] bg-gray-300"
+        className="popup w-4/12  rounded h-[90vh]"
       >
         <div className="popup-content px-5 flex flex-col">
           <div className="flex justify-between pt-5">
-            <h1 className="text-xl">Add a task</h1>
-            <button className="text-xl" onClick={() => setPopup(false)}>x</button>
+            <h1 style={{color: "var(--text-b)"}} className="text-xl">Add a task</h1>
+            <button style={{color: "var(--text-b)"}} className="text-xl" onClick={() => setPopup(false)}>x</button>
           </div>
-          <label htmlFor="Title" className="pt-5 " style={{color: "var(--text-s)"}}>
+          <label htmlFor="Title" className="pt-5" style={{color: "var(--text-s)"}} >
             Title
           </label>
           <input
+          style={{backgroundColor: "var(--side-clr)",color: "var(--text-s)"}}
             id="Title"
             type="text"
             placeholder="e.g, study for the test"
-            className="py-3 px-2 rounded"
+            className="py-2 mt-1 px-2 rounded"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <label htmlFor="Date" className="pt-3">
+          <label htmlFor="Date" className="pt-8" style={{color: "var(--text-s)"}}>
             Date
           </label>
           <input
             id="Date"
             type="date"
-            className="py-3 px-2 rounded"
+            style={{backgroundColor: "var(--side-clr)",color: "var(--text-b)"}}
+            className="py-2 mt-1 px-2 rounded"
             value={date}
             onChange={(e) => setDate(e.target.value)}
           />
-          <label htmlFor="Description" className="pt-3">
+          <label htmlFor="Description" className="pt-8" style={{color: "var(--text-s)"}}>
             Description (optional)
           </label>
           <textarea
             name=""
             id="Description"
             cols="30"
-            rows="3"
-            className="rounded"
+            rows="2"
+            className="rounded mt-1 px-2 py-2"
+            style={{backgroundColor: "var(--side-clr)",color: "var(--text-s)"}}
+            placeholder="e.g, study for the test"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
-          <div className="flex gap-2 pt-3">
+          <div className="flex gap-2 pt-8">
             <input type="checkbox" />
-            <h1>Mark as important</h1>
+            <h1 style={{color: "var(--text-s)"}}>Mark as important</h1>
           </div>
           <div className="flex gap-2">
-            <input type="checkbox" />
-            <h1>Mark as complete</h1>
+            <input type="checkbox" style={{backgroundColor: "var(--side-clr)"}}/>
+            <h1 style={{color: "var(--text-s)"}}>Mark as complete</h1>
           </div>
-          <button className="bg-purple-500 rounded py-2 mt-5 text-white"  onClick={()=>{ handleSubmitData(title,date,description,setTitle,setDate,setDescription) ; setPopup(false)  }}>
+          <button className="bg-purple-500 rounded py-2 mt-8 text-white"  onClick={()=>{ handleSubmitData(title,date,description,setTitle,setDate,setDescription) ; setPopup(false)  }}>
             Add a task
           </button>
         </div>
